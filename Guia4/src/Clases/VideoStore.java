163c4b6bfdd4e84659faa33124f21f6fcf1bd689
unknown <NVera92>
2023-04-07 00:48:58 -0300

    videostore inicial

---
+++ Guia4/src/Clases/VideoStore.java
@@ -0,0 +1,20 @@
+import Clases.Clientes;
+import Clases.Peliculas;
+
+import java.util.ArrayList;
+
+public class VideoStore{
+
+    private ArrayList<Peliculas> stockPeliculas = new ArrayList<>();
+
+    //ArrayList<Clientes> listadoClientes = new ArrayList<>();
+
+    public void agregarPelicula(){
+        this.stockPeliculas.add(new Peliculas());
+    }
+
+
+
+
+
+}
